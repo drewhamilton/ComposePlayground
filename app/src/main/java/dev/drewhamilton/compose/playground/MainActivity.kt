20a69d0b99378efb3246f577e566dd574db48370
Drew Hamilton <drew.hamilton@backbase.com>
2021-02-10 15:51:13 +0100

    Initialize project

---
+++ app/src/main/java/dev/drewhamilton/compose/playground/MainActivity.kt
@@ -0,0 +1,35 @@
+package dev.drewhamilton.compose.playground
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
+import com.google.android.material.composethemeadapter.MdcTheme
+import dev.chrisbanes.accompanist.insets.ProvideWindowInsets
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ProvideWindowInsets {
+                MdcTheme {
+                    Surface(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Text(
+                            text = "Compose Playground",
+                            style = MaterialTheme.typography.h5
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
