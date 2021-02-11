9a8db7a9af79b71bf0643ec4b8919dd769474aaf
Drew Hamilton <github@drewhamilton.dev>
2021-02-11 11:17:40 +0100

    Apply Poko 0.7.0
    Add commented-out manual hashCode which works around bug

---
+++ app/src/main/java/dev/drewhamilton/compose/playground/PokoItem.kt
@@ -0,0 +1,10 @@
+package dev.drewhamilton.compose.playground
+
+import dev.drewhamilton.poko.Poko
+
+@Poko class PokoItem(
+    val id: String
+) {
+    // Uncomment this and compilation is fixed:
+    override fun hashCode(): Int = id.hashCode()
+}
