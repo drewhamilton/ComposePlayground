3e0e6d2a5f6d548049d80efdc9a6b8af81bc4025
Drew Hamilton <github@drewhamilton.dev>
2021-02-11 15:03:55 +0100

    Comment out manual hashCode

--- app/src/main/java/dev/drewhamilton/compose/playground/PokoItem.kt
+++ app/src/main/java/dev/drewhamilton/compose/playground/PokoItem.kt
@@ -6,5 +6,5 @@ import dev.drewhamilton.poko.Poko
     val id: String
 ) {
     // Uncomment this and compilation is fixed:
-    override fun hashCode(): Int = id.hashCode()
+//    override fun hashCode(): Int = id.hashCode()
 }
