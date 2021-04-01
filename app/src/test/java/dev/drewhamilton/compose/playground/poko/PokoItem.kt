cec1fcf5cb5a2891089b4cf5eb5c75c6d40c92a7
Drew Hamilton <github@drewhamilton.dev>
2021-04-01 12:31:33 +0200

    Compose 1.0.0-beta03 (#2)
    * Compose 1.0.0-beta03
    * Accompanist 0.7.0, AGP 7.0.0-alpha12, Poko 0.7.3
    * Test Poko in JVM tests rather than MainActivity

---
+++ app/src/test/java/dev/drewhamilton/compose/playground/poko/PokoItem.kt
@@ -0,0 +1,8 @@
+package dev.drewhamilton.compose.playground.poko
+
+import dev.drewhamilton.poko.Poko
+
+@Poko class PokoItem(
+    @Suppress("unused") val id: String,
+    @Suppress("unused") val count: Int = 1,
+)
